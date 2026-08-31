d1ec54cb50a9eee520492e9608ccf4abcc7d6577
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-31 16:25:16 +1000

    Add debounced local lighting inputs

--- library/include/bike/lighting_local_control.hpp
+++ library/include/bike/lighting_local_control.hpp
@@ -5,21 +5,41 @@
 
 namespace bike {
 
+class DebouncedInput {
+public:
+    bool update(bool raw, std::uint32_t now_ms, std::uint32_t debounce_ms);
+    bool value() const { return stable_; }
+
+private:
+    bool raw_{false};
+    bool stable_{false};
+    bool initialized_{false};
+    std::uint32_t changed_at_ms_{0};
+};
+
 class LightingLocalControl {
 public:
     LightingLocalControl(LightingController& controller, LightingHardware& outputs, LightingInputs& inputs)
         : controller_(controller), outputs_(outputs), inputs_(inputs) {}
 
     void set_blink_half_period_ms(std::uint32_t value) { blink_half_period_ms_ = value; }
+    void set_input_debounce_ms(std::uint32_t value) { input_debounce_ms_ = value; }
     void service(std::uint32_t now_ms);
 
 private:
     bool write_if_changed(LightingOutput output, bool enabled);
     bool local_indicator_active() const;
+    void sample_inputs(std::uint32_t now_ms);
 
     LightingController& controller_;
     LightingHardware& outputs_;
     LightingInputs& inputs_;
+    DebouncedInput left_{};
+    DebouncedInput right_{};
+    DebouncedInput front_brake_{};
+    DebouncedInput rear_brake_{};
+    DebouncedInput high_beam_{};
+    std::uint32_t input_debounce_ms_{25};
     std::uint32_t blink_half_period_ms_{500};
     std::uint32_t last_blink_toggle_ms_{0};
     bool blink_on_{true};
