c2c257e96c2cc1b4239eee39fb1bcc013578f271
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-31 16:25:06 +1000

    Split front and rear brake lighting inputs

--- library/include/bike/lighting_inputs.hpp
+++ library/include/bike/lighting_inputs.hpp
@@ -8,8 +8,13 @@ public:
 
     virtual bool left_indicator_requested() const = 0;
     virtual bool right_indicator_requested() const = 0;
-    virtual bool brake_active() const = 0;
+    virtual bool front_brake_active() const = 0;
+    virtual bool rear_brake_active() const = 0;
     virtual bool high_beam_requested() const = 0;
+
+    bool brake_active() const {
+        return front_brake_active() || rear_brake_active();
+    }
 };
 
 } // namespace bike
