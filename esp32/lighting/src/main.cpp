66a7c1495b5f7c11f3f4aac69151d2d4a64e1e07
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-31 16:22:18 +1000

    Run local lighting controls on ESP32

--- esp32/lighting/src/main.cpp
+++ esp32/lighting/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "bike/lighting_controller.hpp"
+#include "bike/lighting_local_control.hpp"
 #include "lighting_platform.hpp"
 
 namespace {
@@ -7,8 +8,10 @@ namespace {
 HardwareSerial NetworkSerial(2);
 lighting_platform::ArduinoSerialTransport transport(NetworkSerial);
 lighting_platform::GpioLightingHardware hardware;
+lighting_platform::GpioLightingInputs inputs;
 bike::BikeNode node(bike::NodeAddress::Lighting, transport);
 bike::LightingController controller(node, hardware);
+bike::LightingLocalControl local_control(controller, hardware, inputs);
 
 } // namespace
 
@@ -17,6 +20,7 @@ void setup() {
     delay(50);
 
     hardware.begin();
+    inputs.begin();
 
     NetworkSerial.begin(
         lighting_platform::kNetworkBaud,
@@ -26,11 +30,14 @@ void setup() {
 
     node.set_ack_timeout_ms(250);
     node.set_max_retries(3);
+    local_control.set_blink_half_period_ms(500);
 
     Serial.println("lighting-controller: booted");
 }
 
 void loop() {
-    controller.service(millis());
+    const auto now_ms = millis();
+    controller.service(now_ms);
+    local_control.service(now_ms);
     delay(1);
 }
