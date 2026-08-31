921c6decb0a904421b758c0d9daf8b0784049bf7
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-31 19:38:10 +1000

    Track malformed stream frames

--- library/include/bike/stream_parser.hpp
+++ library/include/bike/stream_parser.hpp
@@ -10,11 +10,13 @@ class StreamParser {
 public:
     bool push(std::uint8_t byte, Packet& out_packet);
     void reset();
+    std::uint32_t rejected_frames() const { return rejected_frames_; }
 
 private:
     std::array<std::uint8_t, kMaxFrameSize> buffer_{};
     std::size_t length_{0};
     std::size_t expected_{0};
+    std::uint32_t rejected_frames_{0};
 };
 
 } // namespace bike
